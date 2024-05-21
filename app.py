6c58b5db9354dacec2acdc52a386c42eb2a480bb
Chris Bryan <chris.bryan2@nhs.net>
2024-05-21 10:38:13 +0100

    status

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from configparser import ConfigParser, NoOptionError, NoSectionError
 import os
 import boto3
 from pathlib import Path
-from tkinter import Tk, Label, Entry, Button
+from tkinter import Frame, Tk, Label, Entry, Button
 
 
 # boto3.set_stream_logger('')  # debug
@@ -66,6 +66,34 @@ def reboot_instance(instance_id):
         print(f"Error rebooting instance {instance_id}: {e}")
 
 
+def get_instance_state(instance_id):
+    """
+    Retrieves the current state of the EC2 instance.
+    """
+    try:
+        response = ec2_client.describe_instances(InstanceIds=[instance_id])
+        return response["Reservations"][0]["Instances"][0]["State"]["Name"]
+    except Exception as e:
+        print(f"Error getting instance state: {e}")
+
+
+status_color_dict = {
+    "stopped": "red",
+    "running": "green",
+}
+
+
+def update_instance_status(instance_id_text_field: Label, status_button: Button):
+    """
+    Updates the status based on the current state.
+    """
+    status = get_instance_state(instance_id_text_field.get())
+    if status:
+        status_button.config(text=status, background=status_color_dict.get(status, "grey"))
+    else:
+        status_button.config(text="Error retrieving status")
+
+
 def perform_action(instance_id, action):
     if action == "start":
         start_instance(instance_id.get())
@@ -91,18 +119,32 @@ instance_id = Entry(root)
 instance_id.insert(0, get_instance_id())
 instance_id.pack()
 
+button_frame = Frame(root)
+button_frame.pack()
+
 # Action buttons
-start_button = Button(root, text="Start", command=lambda: perform_action(instance_id, "start"))
+start_button = Button(button_frame, text="Start", command=lambda: perform_action(instance_id, "start"))
 start_button.pack(side="left")
 
-stop_button = Button(root, text="Stop", command=lambda: perform_action(instance_id, "stop"))
+stop_button = Button(button_frame, text="Stop", command=lambda: perform_action(instance_id, "stop"))
 stop_button.pack(side="left")
 
-reboot_button = Button(root, text="Reboot", command=lambda: perform_action(instance_id, "reboot"))
+reboot_button = Button(button_frame, text="Reboot", command=lambda: perform_action(instance_id, "reboot"))
 reboot_button.pack(side="left")
 
-save_id_button = Button(root, text="Save ID", command=lambda: perform_action(instance_id, "save_id"))
+save_id_button = Button(button_frame, text="Save ID", command=lambda: perform_action(instance_id, "save_id"))
 save_id_button.pack(side="left")
+
+# Status
+status_button = Button(root)
+status_button.pack()
+
+
+# Poll status
+def poll_instance_status():
+  update_instance_status(instance_id, status_button)
+  root.after(3000, poll_instance_status)  # Call again after 3 seconds
+poll_instance_status()
 
 # Run the main event loop
 root.mainloop()
